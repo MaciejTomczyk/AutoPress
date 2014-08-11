eefeed1da6a5d478ce1f2dd044273a1696e15175
Andrey <synecraft@gmail.com>
2014-08-11 20:48:24 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace AutoPress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 0, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.55556F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.44444F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(212, 207);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(2, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Location = new System.Drawing.Point(2, 143);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(2, 126);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tick timer in ms:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(2, 69);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(208, 46);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(2, 187);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.linkLabel1.Location = new System.Drawing.Point(2, 165);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(208, 20);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 207);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "AutoPress 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace AutoPress
+{
+    public partial class Form1 : Form
+    {
+        int interval = 0;
+        byte keyGlobal = 0;
+    	int oldInterval = 0;
+    	int oldInterval2 = 0;
+    	int pressTimes = 0;
+
+    	Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll", SetLastError = true)]
+
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        //const int VK_UP = 0x26; //up key
+        //const int VK_DOWN = 0x28;  //down key
+        //const int VK_LEFT = 0x25;
+        //const int VK_RIGHT = 0x27;
+        //const int VK_LWIN = 0x5B;
+        //const int H_KEY = 0x48;
+
+        const int VK_OEM_3 = 0x55;
+		
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+
+        void press(int theKey)
+        {
+            keybd_event((byte)theKey, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0 );
+            Thread.Sleep(r.Next(1,15));
+            keybd_event((byte)theKey, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+        void press(byte theKey)
+        {
+            keybd_event(theKey, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0 );
+            Thread.Sleep(r.Next(1,15));
+            keybd_event(theKey, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled == true)
+            {
+        	timer1.Enabled = false;
+            richTextBox1.Text = richTextBox1.Text + "Stopped! ";
+            textBox2.Enabled = true;
+            this.richTextBox1.SelectionStart = this.richTextBox1.Text.Length;
+            this.richTextBox1.ScrollToCaret();
+            textBox1.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        	if(timer1.Enabled == false)
+        	{
+        	    pressTimes = 0;
+                this.Text = ("Ticks: " + pressTimes);
+                timer1.Enabled = true;
+                richTextBox1.Text = richTextBox1.Text + "Started! ";
+                textBox2.Enabled = false;
+                int x = 0;
+            
+            // Interval check
+                    if (textBox2.Text != "" && int.TryParse(textBox2.Text, out x))
+                    {
+                        timer1.Interval = Math.Abs(int.Parse(textBox2.Text));
+                        textBox2.Text = Math.Abs(int.Parse(textBox2.Text)).ToString();	
+                        oldInterval = Math.Abs(int.Parse(textBox2.Text));
+                        oldInterval2 = Math.Abs(int.Parse(textBox2.Text)) + 1600;
+                    }
+            else
+            {
+                MessageBox.Show("Invalid value was entered, selecting 1000");
+                timer1.Interval = 1000;
+                textBox2.Text = "1000";
+                oldInterval = 1000;
+                oldInterval2 = 1000 + 1600;
+            }
+            
+            // Key check
+            if (textBox1.Text == "" | textBox1.Text == ("Using default(U)"))
+            {
+                textBox1.Text = ("Using default(U)");
+                textBox1.Enabled = false;
+            }
+            else
+            {
+            	try
+            	{
+            		keyGlobal = Convert.ToByte(textBox1.Text, 16);
+            		textBox1.Enabled = false;
+            	}
+            	catch(Exception ex)
+            	{
+            		button1.PerformClick();
+            		textBox1.Text = "";
+            		MessageBox.Show(ex.Message);
+            	}
+            		
+            }
+            this.richTextBox1.SelectionStart = this.richTextBox1.Text.Length;
+            this.richTextBox1.ScrollToCaret();
+        }
+       }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            richTextBox1.Text = "Console: ";
+            textBox2.Text = "1000";
+            linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            linkLabel1.Text = "Key in byte value(see link)";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pressTimes++;
+            this.richTextBox1.SelectionStart = this.richTextBox1.Text.Length;
+            this.richTextBox1.ScrollToCaret();
+            this.Text = ("Ticks: " + pressTimes);			
+			interval = r.Next(oldInterval, oldInterval2);
+			richTextBox1.Text = richTextBox1.Text + timer1.Interval + " ";
+			timer1.Interval = interval;
+            if (textBox1.Text == "" | textBox1.Text == "Using default(U)")
+            {
+            	press(VK_OEM_3);
+            }
+            else
+            {
+            	press(keyGlobal);
+            }
+        }
+        private void linkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+    	{
+            this.linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://msdn.microsoft.com/en-us/library/dd375731%28v=vs.85%29.aspx");
+    	}
+        
+        void RichTextBox1TextChanged(object sender, EventArgs e)
+        {
+        	this.richTextBox1.SelectionStart = this.richTextBox1.Text.Length;
+            this.richTextBox1.ScrollToCaret();
+        }
+    }
+}
